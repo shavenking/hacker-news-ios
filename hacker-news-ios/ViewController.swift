c7a1c4406765ad0b5b2e23e637b2148cc7a66c2f
shavenking <shavenking@gmail.com>
2018-04-12 14:05:29 +0800

    Initial

---
+++ hacker-news-ios/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  hacker-news-ios
+//
+//  Created by Andy Tsai on 2018/4/12.
+//  Copyright © 2018 Andy Tsai. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .blue
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
